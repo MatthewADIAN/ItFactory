41b8d1d99dad541f705d510397116de875db0622
MatthewADIAN <672018055@student.uksw.edu>
2020-11-27 15:03:55 +0700

    Update tampilProduk.blade.php

--- resources/views/tampilProduk.blade.php
+++ resources/views/tampilProduk.blade.php
@@ -5,15 +5,61 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Produk</title>
 </head>
-<body>
-<table border='1'>
-    <tr>
-        <th>Id</th>
-        <th>Kode Produk</th>
-        <th>Nama Produk</th>
-        <th>Harga Produk</th>
-        <th>Action</th>
-    </tr>
+<body style="background-color: #C7AC92">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+
+    <nav class="navbar navbar-light bg-light fixed-top">
+            <a class="navbar-brand">It Factory Website</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/lowongan/show">Lowongan</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link">Produk</a>
+                    </li>
+                    <!--
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Profile
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" th:href="@{/address}">Address</a>
+                            <a class="dropdown-item" th:href="@{/basic}">Basic</a>
+                            <a class="dropdown-item" th:href="@{/contact}">Contact</a>
+                            <a class="dropdown-item" th:href="@{/currentoccupation}">Current Occupation</a>
+                            <a class="dropdown-item" th:href="@{/education}">Education</a>
+                        </div>
+                    </li>
+                    -->
+                </ul>
+            </div>
+        </nav>
+
+        <br><br><br>
+
+<table border='1' class="table table-striped">
+    <thead class="thead-dark">
+        <tr>
+            <th>Id</th>
+            <th>Kode Produk</th>
+            <th>Nama Produk</th>
+            <th>Harga Produk</th>
+            <th>Action</th>
+        </tr>
+    </thead>
+    <tbody>
     @foreach($produk as $p)
         <tr>
             <td>{{ $p->id }}</td>
@@ -25,6 +71,7 @@
             </td>
         </tr>
     @endforeach
+    </tbody>
     </table>
     <br><br>
     <a href="/produk/insert">Insert Produk</a>
